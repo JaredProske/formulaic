14378b9fc16b8e51282fcf92bf02cb20702ac94f
Jared Proske <jaredproske@Jareds-MacBook-Air.local>
2020-08-16 13:29:43 -0700

    make test better

--- src/lib/randomizer/randomizer.test.ts
+++ src/lib/randomizer/randomizer.test.ts
@@ -1,53 +1,53 @@
 import Randomizer from '.';
 
 describe('Get a random hex color', () => {
-    it('returns a valid hex color', () => {
-        const rand = new Randomizer();
+  it('returns a valid hex color', () => {
+    const rand = new Randomizer();
 
-        for (let i = 0; i < 1000; i++) {
-            const randomColor = rand.getRandomHexColor();
-            const testResult = /^#[0-9A-F]{6}$/i.test(randomColor);
+    for (let i = 0; i < 1000; i++) {
+      const randomColor = rand.getRandomHexColor();
+      const testResult = /^#[0-9A-F]{6}$/i.test(randomColor);
 
-            expect(testResult).toBe(true);
-        }
-    });
+      expect(testResult).toBe(true);
+    }
+  });
 
-    it('can be seeded', () => {
-        const coolSeed = 'Welcome to the party, pal.';
-        let subject = new Randomizer(coolSeed);
-        const firstColors = [];
+  it('can be seeded', () => {
+    const coolSeed = 'Welcome to the party, pal.';
+    let subject = new Randomizer(coolSeed);
+    const firstColors = [];
 
-        for (let i = 0; i < 1000; i++) {
-            firstColors.push(subject.getRandomHexColor());
-        }
+    for (let i = 0; i < 1000; i++) {
+      firstColors.push(subject.getRandomHexColor());
+    }
 
-        subject = new Randomizer(coolSeed);
-        const secondColors = [];
+    subject = new Randomizer(coolSeed);
+    const secondColors = [];
 
-        for (let i = 0; i < 1000; i++) {
-            secondColors.push(subject.getRandomHexColor());
-        }
+    for (let i = 0; i < 1000; i++) {
+      secondColors.push(subject.getRandomHexColor());
+    }
 
-        expect(secondColors).toEqual(firstColors);
-    });
+    expect(secondColors).toEqual(firstColors);
+  });
 });
 
 describe('Get a random bool', () => {
-    it('can be seeded', () => {
-        const coolSeed = 'Welcome to the party, pal.';
-        let rand = new Randomizer(coolSeed);
-        const firstBools = [];
+  it('can be seeded', () => {
+    const coolSeed = 'Welcome to the party, pal.';
+    let rand = new Randomizer(coolSeed);
+    const firstBools = [];
 
-        for (let i = 0; i < 10000; i++) {
-            firstBools.push(rand.getRandomBool());
-        }
+    for (let i = 0; i < 10000; i++) {
+      firstBools.push(rand.getRandomBool());
+    }
 
-        rand = new Randomizer(coolSeed);
-        const secondBools = [];
+    rand = new Randomizer(coolSeed);
+    const secondBools = [];
 
-        for (let i = 0; i < 10000; i++) {
-            secondBools.push(rand.getRandomBool());
-        }
-        expect(secondBools).toEqual(firstBools);
-    });
+    for (let i = 0; i < 10000; i++) {
+      secondBools.push(rand.getRandomBool());
+    }
+    expect(secondBools).toEqual(firstBools);
+  });
 });
